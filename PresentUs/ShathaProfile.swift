e879c4312e581aa359f53ac8ef768598d83921a2
ShathaMK <shathamukhaild@gmail.com>
2024-09-29 11:50:39 +0300

    Initial Commit

---
+++ PresentUs/ShathaProfile.swift
@@ -0,0 +1,37 @@
+//
+//  ShathaProfile.swift
+//  PresentUs
+//
+//  Created by Shatha Almukhaild on 26/03/1446 AH.
+//
+
+//
+//  Profile.swift
+//  SwiftUIFlashCard
+//
+//  Created by Shatha Almukhaild on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+struct ShathaProfile: View {
+    var body: some View {
+        VStack(spacing:10){
+            
+            Image("Horse").resizable().frame(width:200,height: 200).clipShape(.circle).overlay(    Circle()
+                .stroke(Color.brown, lineWidth: 6)
+)
+       
+            Text("Shatha Almukhaild").font(.title).fontWeight(.medium)            .frame(maxWidth: .infinity, alignment: .leading).padding().foregroundStyle(.brown)
+
+            Text("Hi devs, I'm a computer science gradutes from Alahsa. I enjoy coding and designing. I recently moved to Riyadh to pursue my dream to become an application developer.").font(.body) .frame(maxWidth: .infinity, alignment: .leading).padding()
+            
+            
+        }
+    }
+}
+
+#Preview {
+    ShathaProfile()
+}
+
