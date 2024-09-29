01ba1b44df0e703ec31900e0200d1fdecf9df73a
shfa <shefaa.eidaroos@gmail.com>
2024-09-29 12:18:38 +0300

    added my profile

---
+++ PresentUs/ShfaProfile.swift
@@ -0,0 +1,18 @@
+//
+//  ShfaProfile.swift
+//  PresentUs
+//
+//  Created by Whyyy on 29/09/2024.
+//
+
+import SwiftUI
+
+struct ShfaProfile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShfaProfile()
+}
