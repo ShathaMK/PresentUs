416dfe0306111d564bcb271b02e2ad83863c902c
Raghad Almarri <raghad55.11@icloud.com>
2024-09-29 12:36:27 +0300

    added my last name

--- PresentUs/Raghad profile.swift
+++ PresentUs/Raghad profile.swift
@@ -16,7 +16,7 @@ struct Raghad_profile: View {
                 .clipShape(Circle())
                 .overlay(Circle().stroke(Color.orange, lineWidth: 4))
                 
-            Text("Raghad")
+            Text("Raghad Almarri")
                 .font(.title)
                 .fontWeight(.bold)
                 .foregroundColor(Color.orange)
