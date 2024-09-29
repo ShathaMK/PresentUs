912d9be380c9210b22db9e66c8a824737056a061
Yomna <yomna.ar.eisa@gmail.com>
2024-09-29 12:27:07 +0300

    changed color

--- PresentUs/YomnaProfile.swift
+++ PresentUs/YomnaProfile.swift
@@ -15,7 +15,7 @@ struct YomnaProfile: View {
                     Circle().stroke(Color.pink, lineWidth: 5))
             
             Text("Yomna Eisa").font(.title.bold())
-                .foregroundColor(.pink)
+                .foregroundColor(.green)
                 .opacity(1.5)
                 .frame(maxWidth: .infinity, alignment: .leading)
                 .padding(1.5)
