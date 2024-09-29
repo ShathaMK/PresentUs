1a0312a0f451779fc36df4554dd8280bad5755d0
ShathaMK <shathamukhaild@gmail.com>
2024-09-29 11:50:39 +0300

    Initial Commit

---
+++ PresentUs/PresentUsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PresentUsApp.swift
+//  PresentUs
+//
+//  Created by Shatha Almukhaild on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct PresentUsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
